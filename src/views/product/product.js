08fad5eb9032e5b4d5e915bc611361f53fc4bc9e
dahyun <jigunt9014@gmail.com>
2024-02-24 22:25:58 +0900

    Feat: 상품전체 html,js,css 수정

--- src/views/product/product.js
+++ src/views/product/product.js
@@ -3,12 +3,7 @@ let ServerData = [
     { author: '이지연, 현내음', title: '빛과 향기를 담다', img: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbLPhlN%2FbtsEyde34xx%2F0pf18lIU5kNPkpWNJ9Bezk%2Fimg.png', infolink: 'https://art-map.co.kr/exhibition/view.php?idx=29641', locations: '하람갤러리/서울', startDate: '2024.2.8', endDate: '2024.2.29' },
     { author: '백주미', title: 'COMFORT ZONE', img: 'https://cdn.imweb.me/thumbnail/20240213/d6d9a9f57fc6a.jpg', infolink: 'https://art-map.co.kr/exhibition/view.php?idx=29641', locations: '하람갤러리/서울', startDate: '2024.2.8', endDate: '2024.2.29' },
     { author: '음하영', title: 'MY LITTLE', img: 'https://static.wixstatic.com/media/ab9818_8f4201bf4ce84390bc0eb2111aa4a5c7~mv2.png/v1/fill/w_714,h_714,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/KakaoTalk_20240215_183219107_01.png', locations: '하람갤러리/서울', startDate: '2024.2.8', endDate: '2024.2.29' },
-    { author: '송현화', title: 'LUXURY PLASTIC : #3 Magic Airs', img: 'http://www.gallerywe.com/wp-content/uploads/2024/02/1.Magic-Airs-130.3x130.jpg', locations: '하람갤러리/서울', startDate: '2024.2.8', endDate: '2024.2.29' },
-
-    { author: '송현화', title: 'LUXURY PLASTIC : #3 Magic Airs', img: 'https://static.wixstatic.com/media/9f5a27_fd74172ba0624daa956a0e34b188789d~mv2.jpg/v1/fill/w_1024,h_1024,al_c,q_85,enc_auto/9f5a27_fd74172ba0624daa956a0e34b188789d~mv2.jpg', locations: '하람갤러리/서울', startDate: '2024.2.8', endDate: '2024.2.29' },
-    { author: '이지연, 현내음', title: '빛과 향기를 담다', img: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbLPhlN%2FbtsEyde34xx%2F0pf18lIU5kNPkpWNJ9Bezk%2Fimg.png', infolink: 'https://art-map.co.kr/exhibition/view.php?idx=29641', locations: '하람갤러리/서울', startDate: '2024.2.8', endDate: '2024.2.29' },
-    { author: '백주미', title: 'COMFORT ZONE', img: 'https://cdn.imweb.me/thumbnail/20240213/d6d9a9f57fc6a.jpg', infolink: 'https://art-map.co.kr/exhibition/view.php?idx=29641', locations: '하람갤러리/서울', startDate: '2024.2.8', endDate: '2024.2.29' },
-    { author: '음하영', title: 'MY LITTLE', img: 'https://static.wixstatic.com/media/ab9818_8f4201bf4ce84390bc0eb2111aa4a5c7~mv2.png/v1/fill/w_714,h_714,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/KakaoTalk_20240215_183219107_01.png', locations: '하람갤러리/서울', startDate: '2024.2.8', endDate: '2024.2.29' },
+    { author: '송현화', title: 'LUXURY PLASTIC : #3 Magic Airsdsdsdsdsdsd', img: 'http://www.gallerywe.com/wp-content/uploads/2024/02/1.Magic-Airs-130.3x130.jpg', locations: '하람갤러리/서울', startDate: '2024.2.8', endDate: '2024.2.29' },
 ]
 
 // 변수 생성
@@ -17,7 +12,7 @@ const numRows = Math.ceil(ServerData.length / numColumn); //전체 행의 개수
 
 // 전시회 목록 삽입
 for (let i = 0; i < numRows; i++) {
-    let columnsContainer = document.createElement('div');
+    const columnsContainer = document.createElement('div');
     columnsContainer.classList.add('columns'); 
 
     for (let j = 0; j < numColumn; j++) {
--- src/views/product/scroll.js
+++ src/views/product/scroll.js
@@ -1,41 +1,46 @@
+//DB와 데이터 연결시 삭제?
 const list = document.querySelector('.exhibition-list'); // 새로운 요소를 추가할 때 사용될 부모 요소
 const items = document.querySelectorAll('.column'); // 현재 화면에 출력되고 있는 아이템을 담은 배열
-
-const listEnd = document.querySelector('#listEnd'); // 관찰할 대상(요소)
+//
 
 const options = {
-    root: null, // 뷰포트를 기준으로 타켓의 가시성 검사
-    rootMargin: '0px 0px 0px 0px', //감지영역 확장
-    threshold: 0, // 타켓의 가시성 0%일 때 옵저버 실행
+    root: null, 
+    rootMargin: '0px 0px 10px 0px', 
+    threshold: 0.7, 
 };
 
 let page = 1;
+let observer;
 
-//Intersection Observer의 콜백함수
-//entries는 Intersection Observer가 관찰하는 대상들의 집합
-///isIntersecting 대상 요소가 교차되었는지 여부를 나타내는 불리언
-const onIntersect = async (entries, observer) => {
-    //만약 현재 반복 중인 요소가 Intersection Observer의 타겟 요소와 교차하고 있을 때 내부의 코드를 실행
-    entries.forEach(async (entry) => { //루프문 비동기 처리
-        if (entry.isIntersecting) { 
-            page++;
-            // 새로운 내용 가져오는 함수
-            await addNewContent(); 
-        } 
-    });
-};
-
-const observer = new IntersectionObserver(onIntersect, options); // IntersectionObserver 생성
-observer.observe(listEnd); // 관찰할 대상(요소) 등록
-
-
-// 백엔드 통신X 일단 데이터 복사해서 사용, 백엔드 통신시 비동기 문법 사용 데이터 받은후 처리
+// 새로운 내용 가져오는 함수
+//cloneNode 대신 DB 데이터 연결
 const addNewContent = () => {
     const newRow = document.createElement('div');
-    newRow.classList.add('columns'); // 새로운 열 추가
+    newRow.classList.add('columns'); 
     items.forEach(item => {
         const newItem = item.cloneNode(true);
         newRow.appendChild(newItem); // 새로운 열에 아이템 추가
     });
     list.appendChild(newRow); // 새로운 열을 전체 리스트에 추가
 };
+
+// Intersection Observer 콜백함수
+const onIntersect = async (entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            page++;
+            //관찰대상은 새로운 데이터를 가져올 때마다 변해야함
+            addNewContent();
+            // 기존 Observer 해제
+            observer.disconnect();
+            // 새로운 listEnd 요소 가져오기
+            listEnd = list.lastElementChild;
+            // 다시 Intersection Observer로 관찰 대상을 설정
+            observer.observe(listEnd);
+        }
+    });
+};
+
+let listEnd = list.lastElementChild; // 리스트의 끝을 나타내는 요소
+observer = new IntersectionObserver(onIntersect, options);
+observer.observe(listEnd);
