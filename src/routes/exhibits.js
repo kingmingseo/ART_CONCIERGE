b65e8ba9d58ee43846e26511e84926d86ae66468
goeun kim <rlarhdms6828@naver.com>
2024-02-25 17:52:36 +0900

    Feat: 카테고리별 전시 조회

--- src/routes/exhibits.js
+++ src/routes/exhibits.js
@@ -1,14 +1,29 @@
 const { Router } = require('express');
 const router = Router();
 
-const { Exhibit } = require('../db');
+const { Exhibit, Category } = require('../db');
 
-// 전시 리스트 조회 (작가, 이미지, 전시제목, 날짜, 카테고리만)
+// 전시 리스트 조회 (작가, 이미지, 전시제목, 날짜, 카테고리만) + 카테고리도 함께 
 router.get('/', async(req, res, next) => {
     try {
         const exhibits = await Exhibit.find({})
             .select('author image exhibitName startDate endDate category')
-            .populate('category', 'category'); // 카테고리 이름 가져오기
+            .populate('category', 'category'); // 전시의 카테고리 이름 가져오기
+        const categories = await Category.find({}); // 모든 카테고리 이름 가져오기
+        res.json({ exhibits,categories });
+    } catch (err) {
+        res.json(err);
+    }
+});
+
+// 카테고리별 전시 리스트 조회 (작가, 이미지, 전시제목, 날짜, 카테고리만) + 카테고리도 함께 
+router.get('/:categoryId', async(req, res, next) => {
+    try {
+        const category  = req.params.categoryId
+        const exhibits = await Exhibit.find({ category })
+            .select('author image exhibitName startDate endDate category')
+            .populate('category', 'category'); // 전시의 카테고리 이름 가져오기
+        // const categories = await Category.find({}); // 모든 카테고리 이름 가져오기
         res.json(exhibits);
     } catch (err) {
         res.json(err);
