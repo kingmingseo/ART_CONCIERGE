119e56a8569aca7fddfb3d7ddd4680b67c7fb88e
dahyun <jigunt9014@gmail.com>
2024-02-26 12:34:48 +0900

    Feat:JS 파일 수정

--- src/views/product/product.js
+++ src/views/product/product.js
@@ -6,9 +6,8 @@ let ServerData = [
     { author: '송현화', title: 'LUXURY PLASTIC : #3 Magic Airs', img: 'http://www.gallerywe.com/wp-content/uploads/2024/02/1.Magic-Airs-130.3x130.jpg', locations: '하람갤러리/서울', startDate: '2024.2.8', endDate: '2024.2.29' },
 
     { author: 'dadsdadsdadsd', title: '빛과 향기를 담다', img: 'https://thumb.mt.co.kr/06/2014/02/2014020414202535761_1.jpg/dims/optimize/', infolink: 'https://art-map.co.kr/exhibition/view.php?idx=29641', locations: '하람갤러리/서울', startDate: '2024.2.8', endDate: '2024.2.29' },
-    { author: '백주미', title: 'COMFORT ZONE', img: 'https://cdn.imweb.me/thumbnail/20240213/d6d9a9f57fc6a.jpg', infolink: 'https://art-map.co.kr/exhibition/view.php?idx=29641', locations: '하람갤러리/서울', startDate: '2024.2.8', endDate: '2024.2.29' },
     { author: '음하영', title: 'MY LITTLE', img: 'https://static.wixstatic.com/media/ab9818_8f4201bf4ce84390bc0eb2111aa4a5c7~mv2.png/v1/fill/w_714,h_714,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/KakaoTalk_20240215_183219107_01.png', locations: '하람갤러리/서울', startDate: '2024.2.8', endDate: '2024.2.29' },
-    { author: '송현화', title: 'LUXURY PLASTIC : #3 Magic Airs', img: 'http://www.gallerywe.com/wp-content/uploads/2024/02/1.Magic-Airs-130.3x130.jpg', locations: '하람갤러리/서울', startDate: '2024.2.8', endDate: '2024.2.29' },
+
 ];
 
 // 변수 생성
@@ -22,13 +21,14 @@ const addNewContent = () => {
     for (let i = 0; i < numRows; i++) {
         let columnsContainer = document.createElement('div');
         columnsContainer.classList.add('columns');
-        
+        columnsContainer.classList.add('is-multiline');
+
         for (let j = 0; j < numColumn; j++) {
             let dataIndex = i * numColumn + j;
             if (dataIndex < ServerData.length) {
                 let data = ServerData[dataIndex];
                 let columnContents = `
-                    <div class="column">
+                    <div class="column is-one-quarter">
                         <a href="${data.infolink}">
                             <img src="${data.img}" alt="">
                             <div class="exhibitTitle"><a href="${data.infolink}"><div>${data.author}: ${data.title}</div></a></div>
