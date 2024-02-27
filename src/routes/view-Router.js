5460232985d45f39f74d1411f83e4b1a857de883
goeun kim <rlarhdms6828@naver.com>
2024-02-27 17:35:44 +0900

    Fix: css 연동 오류

--- src/routes/view-Router.js
+++ src/routes/view-Router.js
@@ -3,30 +3,38 @@ const path = require("path");
 
 const viewsRouter = express.Router();
 
-// 파일명과 html 폴더명 일치 필요
-// 페이지별 html, css, js 파일 라우팅
-
 viewsRouter.use("/", serveStatic("main")); 
-// viewsRouter.use("/admin-Category", serveStatic("admin-Category"));
-viewsRouter.use("/carts", serveStatic("cart"));
-// viewsRouter.use("/itemDetail", serveStatic("productDetail"));
+
+viewsRouter.use("/admin/categories", serveStatic("admin-Category"));
+viewsRouter.use("/admin/orders", serveStatic("admin-Order"));
+viewsRouter.use("/admin/exhibits", serveStatic("admin-Product"));
+
 viewsRouter.use("/exhibits", serveStatic("product"));
-viewsRouter.use("/auth", serveStatic("login"));
-viewsRouter.use("/users", serveStatic("mypage"));
+// viewsRouter.use("/productDetail", serveStatic("productDetail"));
+
+viewsRouter.use("/carts", serveStatic("cart"));
 viewsRouter.use("/orders", serveStatic("order"));
 // viewsRouter.use("/orderComplete", serveStatic("orderComplete"));
-viewsRouter.use("/signup", serveStatic("registration"));
 
+viewsRouter.use("/auth/join", serveStatic("registration"));
+viewsRouter.use("/auth", serveStatic("login"));
+viewsRouter.use("/users", serveStatic("mypage"));
 
-viewsRouter.use("/", serveStatic(""));
+viewsRouter.use("/", serveStatic("")); // 기본 경로
 
 // ${resource}.html -> 기본 파일로 설정
 function serveStatic(resource) {
-    const resourcePath = path.join(__dirname, `../views/${resource}`);
-    const option = { index: `${resource}.html` };
+    let option;
+    let resourcePath;
+
+    if (resource.includes('admin')) {
+        resourcePath = path.join(__dirname, `../views/admin/${resource}`);
+    } else {
+        resourcePath = path.join(__dirname, `../views/${resource}`);
+    }
+    option = { index: `${resource}.html` };
 
     return express.static(resourcePath, option);
 }
 
 module.exports = viewsRouter;
-
