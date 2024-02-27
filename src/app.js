523edab0c1248510197aec66d181ba04eb444279
yjinn0719 <yjinn0719@gmail.com>
2024-02-27 15:26:20 +0900

    Refactor: app.js 경로 수정

--- src/app.js
+++ src/app.js
@@ -1,59 +1,58 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const session = require('express-session');
-const passport = require('passport'); 
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const session = require("express-session");
+const passport = require("passport");
 
-const indexRouter = require('./routes/index'); // 관리자 페이지 for 전시 관리 
-const adminExhibitsRouter = require('./routes/admin-Router'); // 관리자 페이지 for 전시 관리 
-const adminCategoriesRouter = require('./routes/admin/category'); // 관리자 페이지 for 전시 관리 
-const authRouter = require('./routes/auth'); // 로그인/ 회원가입
-var cartsRouter = require("./routes/carts");
+const indexRouter = require("./routes/index"); // 관리자 페이지 for 전시 관리
+const adminExhibitsRouter = require("./routes/admin-Router"); // 관리자 페이지 for 전시 관리
+const adminCategoriesRouter = require("./routes/admin/category"); // 관리자 페이지 for 전시 관리
+const authRouter = require("./routes/auth"); // 로그인/ 회원가입
+const cartsRouter = require("./routes/carts-routes");
 var usersRouter = require("./routes/users");
-const exhibitRouter = require('./routes/exhibits')
-const adminOrderRouter = require('./routes/admin/adminOrder');  // 관리자 페이지 for 주문
+const exhibitRouter = require("./routes/exhibits");
+const adminOrderRouter = require("./routes/admin/adminOrder"); // 관리자 페이지 for 주문
 
-const orderRouter = require('./routes/orderRouter'); // 주문
+const orderRouter = require("./routes/orderRouter"); // 주문
 
-const getUserFromJWT= require('./middlewares/get-user-from-jwt')
+const getUserFromJWT = require("./middlewares/get-user-from-jwt");
 
-require('./passport')();
+require("./passport")();
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 //로그인을 위한 세션 사용 -> JWT 사용으로 인해 주석처리
-// app.use(session({ 
+// app.use(session({
 //   secret: 'secret',  //세션 식별자 서명
-//   resave: false,  //세션 데이터가 변경되지 않았더라도 계속 저장 
+//   resave: false,  //세션 데이터가 변경되지 않았더라도 계속 저장
 //   saveUninitialized: true  //초기화되지 않은 세션을 저장소에 저장할지 여부 (모든 세션 저장)
 // }));
-app.use(passport.initialize());  //passport 초기화
+app.use(passport.initialize()); //passport 초기화
 // app.use(passport.session()); //passport 세션 사용
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // 배열을 다룰 수 있는 
+app.use(express.urlencoded({ extended: true })); // 배열을 다룰 수 있는
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(getUserFromJWT); // 로그인을 위한 미들웨어
 
-
 app.use("/", indexRouter);
-app.use('/admin/exhibits', adminExhibitsRouter ); //관리자 라우터 (전시)
-app.use('/admin/categories', adminCategoriesRouter); //관리자 라우터 (카테고리)
-app.use('/auth', authRouter); // 로그인 + 회원가입 
+app.use("/admin/exhibits", adminExhibitsRouter); //관리자 라우터 (전시)
+app.use("/admin/categories", adminCategoriesRouter); //관리자 라우터 (카테고리)
+app.use("/auth", authRouter); // 로그인 + 회원가입
 app.use("/carts", cartsRouter);
 app.use("/users", usersRouter);
-app.use("/exhibits", exhibitRouter);// 전시보기 
-app.use('/orders', orderRouter);  // 주문 라우터
-app.use('/admin/orders', adminOrderRouter); //관리자 라우터 (주문)
+app.use("/exhibits", exhibitRouter); // 전시보기
+app.use("/orders", orderRouter); // 주문 라우터
+app.use("/admin/orders", adminOrderRouter); //관리자 라우터 (주문)
 
 /* 
 // catch 404 and forward to error handler
@@ -74,26 +73,26 @@ app.use(function(err, req, res, next) {
  */
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-
 // 몽구스 연결
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DB_CONNECT)
-  .then(() => console.log('MongoDB conected'))
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.DB_CONNECT)
+  .then(() => console.log("MongoDB conected"))
   .catch((err) => {
     console.log(err);
   });
