9c2094bf00c87db466c7c76a1d064121840ccf40
dahyun <jigunt9014@gmail.com>
2024-02-28 19:58:14 +0900

    Feat: js파일 필터 기능 추가

--- src/views/product/product.js
+++ src/views/product/product.js
@@ -14,17 +14,18 @@ window.onload = function () {
 
     //검색시 작품 가져오기
     let searchKeyword = urlParams.get('word');
-    console.log(searchKeyword)
 
-    if (searchKeyword) {
-        fetchFilterData(searchKeyword);
-    } else {
+    if (key === 'word') {
+        searchProductElement(searchKeyword);
+    } else if(key===null){
         insertProductElement();
+    }else{
+        filterProductElement(value)
     }
 }
 
-//검색시 데이터 가져오기
-async function fetchFilterData(keyword) {
+// 검색시 데이터 가져오기
+async function searchProductElement(keyword) {
     // 검색어를 서버에 전송하여 필터링된 데이터를 가져오는 함수
     const res = await fetch(`http://localhost:5001/api/exhibits?search=${keyword}`);
     const searchdata = await res.json();
@@ -58,32 +59,64 @@ async function fetchFilterData(keyword) {
             }
             list.appendChild(columnsContainer);
         }
-    }; 
-} 
+    };
+    
+    // 전시회 목록 초기 로드
+    addNewContent();
+    
+    // 변수 listEnd 초기화
+    let listEnd = list.lastElementChild; // 리스트의 끝을 나타내는 요소
 
+    // Intersection Observer 콜백함수
+    const onIntersect = async (entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                // 관찰대상은 새로운 데이터를 가져올 때마다 변해야함
+                addNewContent();
+                // 기존 Observer 해제
+                observer.disconnect();
+                // 새로운 listEnd 요소 가져오기
+                listEnd = list.lastElementChild;
+                // 다시 Intersection Observer로 관찰 대상을 설정
+                observer.observe(listEnd);
+            }
+        });
+    };
 
-    //필터,전체 데이터 가져오기
-    async function insertProductElement() {
-        const res = await fetch('http://localhost:5001/api/exhibits');
-        const serverdata = await res.json();
+    // 설정 옵션
+    const options = {
+        root: null,
+        rootMargin: '0px 0px 10px 0px',
+        threshold: 0,
+    };
 
-        // numRows 변수 계산
-        const numColumn = 4;
-        const numRows = Math.ceil(serverdata.exhibits.length / numColumn);
+    // Intersection Observer 생성 및 관찰 시작
+    let observer = new IntersectionObserver(onIntersect, options);
+    observer.observe(listEnd);
+}
 
-        const list = document.querySelector('.exhibition-list');
+//전체 데이터 가져오기
+async function insertProductElement() {
+    const res = await fetch('http://localhost:5001/api/exhibits');
+    const serverdata = await res.json();
 
-        const addNewContent = () => {
-            for (let i = 0; i < numRows; i++) {
-                let columnsContainer = document.createElement('div');
-                columnsContainer.classList.add('columns');
-                columnsContainer.classList.add('is-multiline');
+    // numRows 변수 계산
+    const numColumn = 4;
+    const numRows = Math.ceil(serverdata.exhibits.length / numColumn);
 
-                for (let j = 0; j < numColumn; j++) {
-                    let dataIndex = i * numColumn + j;
-                    if (dataIndex < serverdata.exhibits.length) {
-                        let data = serverdata.exhibits[dataIndex];
-                        let columnContents = `
+    const list = document.querySelector('.exhibition-list');
+
+    const addNewContent = () => {
+        for (let i = 0; i < numRows; i++) {
+            let columnsContainer = document.createElement('div');
+            columnsContainer.classList.add('columns');
+            columnsContainer.classList.add('is-multiline');
+
+            for (let j = 0; j < numColumn; j++) {
+                let dataIndex = i * numColumn + j;
+                if (dataIndex < serverdata.exhibits.length) {
+                    let data = serverdata.exhibits[dataIndex];
+                    let columnContents = `
                         <div class="column is-one-quarter">
                             <a href="https://www.naver.com/">
                                 <img src="${data.image}" alt="">
@@ -91,56 +124,133 @@ async function fetchFilterData(keyword) {
                                 <div class="exhibitInfo" id='listEnd'><a href="https://www.naver.com/" class="date">${data.startDate}~${data.endDate}</a></div>
                             </a>
                         </div>`;
-                        columnsContainer.innerHTML += columnContents;
-                        // 카테고리에 따라 페이지에 추가
-                        // if (value === '조각전') {
-                        //     columnsContainer.innerHTML += columnContents;
-                        // } else if (data.category.category === '사진전') {
-                        //     columnsContainer.innerHTML += columnContents;
-                        // } else if (data.category.category === '그림전') {
-                        //     columnsContainer.innerHTML += columnContents;
-                        // } else (vlaue==아무것도 없다면){
-                        //     columnsContainer.innerHTML += columnContents;
-                        // }
-                    }
+                    columnsContainer.innerHTML += columnContents;
+                    // 카테고리에 따라 페이지에 추가
+                    // if (value === '조각전') {
+                    //     columnsContainer.innerHTML += columnContents;
+                    // } else if (data.category.category === '사진전') {
+                    //     columnsContainer.innerHTML += columnContents;
+                    // } else if (data.category.category === '그림전') {
+                    //     columnsContainer.innerHTML += columnContents;
+                    // } else (vlaue==아무것도 없다면){
+                    //     columnsContainer.innerHTML += columnContents;
+                    // }
                 }
-                list.appendChild(columnsContainer);
             }
-        };
+            list.appendChild(columnsContainer);
+        }
+    };
 
-        // Intersection Observer 콜백함수
-        const onIntersect = async (entries, observer) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    //관찰대상은 새로운 데이터를 가져올 때마다 변해야함
-                    addNewContent();
-                    // 기존 Observer 해제
-                    observer.disconnect();
-                    // 새로운 listEnd 요소 가져오기
-                    listEnd = list.lastElementChild;
-                    // 다시 Intersection Observer로 관찰 대상을 설정
-                    observer.observe(listEnd);
+    // Intersection Observer 콜백함수
+    const onIntersect = async (entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                //관찰대상은 새로운 데이터를 가져올 때마다 변해야함
+                addNewContent();
+                // 기존 Observer 해제
+                observer.disconnect();
+                // 새로운 listEnd 요소 가져오기
+                listEnd = list.lastElementChild;
+                // 다시 Intersection Observer로 관찰 대상을 설정
+                observer.observe(listEnd);
+            }
+        });
+    };
+
+    // 설정 옵션
+    const options = {
+        root: null,
+        rootMargin: '0px 0px 10px 0px',
+        threshold: 0,
+    };
+
+    // 전시회 목록 초기 로드
+    addNewContent();
+
+    // 변수 listEnd 초기화
+    let listEnd = list.lastElementChild; // 리스트의 끝을 나타내는 요소
+
+    // Intersection Observer 생성 및 관찰 시작
+    let observer = new IntersectionObserver(onIntersect, options);
+    observer.observe(listEnd);
+}
+
+//필터 데이터 가져오기
+async function filterProductElement(valuedata) {
+    const res = await fetch(`http://localhost:5001/api/exhibits?search=${valuedata}`);
+    const serverdata = await res.json();
+
+    // numRows 변수 계산
+    const numColumn = 4;
+    const numRows = Math.ceil(serverdata.exhibits.length / numColumn);
+
+    const list = document.querySelector('.exhibition-list');
+
+    const addNewContent = () => {
+        for (let i = 0; i < numRows; i++) {
+            let columnsContainer = document.createElement('div');
+            columnsContainer.classList.add('columns');
+            columnsContainer.classList.add('is-multiline');
+
+            for (let j = 0; j < numColumn; j++) {
+                let dataIndex = i * numColumn + j;
+                if (dataIndex < serverdata.exhibits.length) {
+                    let data = serverdata.exhibits[dataIndex];
+                    let columnContents = `
+                        <div class="column is-one-quarter">
+                            <a href="https://www.naver.com/">
+                                <img src="${data.image}" alt="">
+                                <div class="exhibitTitle"><a href="https://www.naver.com/"><div>${data.author}: ${data.exhibitName}</div></a></div>
+                                <div class="exhibitInfo" id='listEnd'><a href="https://www.naver.com/" class="date">${data.startDate}~${data.endDate}</a></div>
+                            </a>
+                        </div>`;
+                    columnsContainer.innerHTML += columnContents;
+                    // 카테고리에 따라 페이지에 추가
+                    // if (value === '조각전') {
+                    //     columnsContainer.innerHTML += columnContents;
+                    // } else if (data.category.category === '사진전') {
+                    //     columnsContainer.innerHTML += columnContents;
+                    // } else if (data.category.category === '그림전') {
+                    //     columnsContainer.innerHTML += columnContents;
+                    // } else (vlaue==아무것도 없다면){
+                    //     columnsContainer.innerHTML += columnContents;
+                    // }
                 }
-            });
-        };
+            }
+            list.appendChild(columnsContainer);
+        }
+    };
 
-        // 설정 옵션
-        const options = {
-            root: null,
-            rootMargin: '0px 0px 10px 0px',
-            threshold: 0,
-        };
+    // Intersection Observer 콜백함수
+    const onIntersect = async (entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                //관찰대상은 새로운 데이터를 가져올 때마다 변해야함
+                addNewContent();
+                // 기존 Observer 해제
+                observer.disconnect();
+                // 새로운 listEnd 요소 가져오기
+                listEnd = list.lastElementChild;
+                // 다시 Intersection Observer로 관찰 대상을 설정
+                observer.observe(listEnd);
+            }
+        });
+    };
 
-        // 전시회 목록 초기 로드
-        addNewContent();
+    // 설정 옵션
+    const options = {
+        root: null,
+        rootMargin: '0px 0px 10px 0px',
+        threshold: 0,
+    };
 
-        // 변수 listEnd 초기화
-        let listEnd = list.lastElementChild; // 리스트의 끝을 나타내는 요소
+    // 전시회 목록 초기 로드
+    addNewContent();
 
-        // Intersection Observer 생성 및 관찰 시작
-        let observer = new IntersectionObserver(onIntersect, options);
-        observer.observe(listEnd);
-    }
+    // 변수 listEnd 초기화
+    let listEnd = list.lastElementChild; // 리스트의 끝을 나타내는 요소
 
-    // 페이지 로드 시 전시회 목록을 추가
-    window.addEventListener('load', insertProductElement);
+    // Intersection Observer 생성 및 관찰 시작
+    let observer = new IntersectionObserver(onIntersect, options);
+    observer.observe(listEnd);
+}
