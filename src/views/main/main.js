63b4679c3a551da14ab423bc1be25214ca6d36e8
kingmingseo <101965138+kingmingseo@users.noreply.github.com>
2024-03-01 12:38:04 +0900

    Design : 자잘한 페이지 디자인 수정

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,19 +1,15 @@
-// indexedDB 생성
-function createDB() {
-  if (window.indexedDB) {
-    const databaseName = "cart";
-    const version = 1;
+const $joinNow = document.querySelector('#joinNow')
+const $artList = document.querySelector('#artList')
 
-    const request = indexedDB.open(databaseName, version);
+$artList.addEventListener('click',()=>{
+  window.location.href = '/exhibits'
+})
 
-    request.onupgradeneeded = function () {
-      request.result.createObjectStore("items", { keyPath: "id" });
-      request.result.createObjectStore("nowBuy", { keyPath: "id" });
-    };
-
-    request.onsuccess = function () {};
-    request.onerror = function (event) {
-      alert(event.target.errorCode);
-    };
+$joinNow.addEventListener('click',()=>{
+  if(token){
+    window.location.href = '/exhibits'
   }
-}
+  else{
+    window.location.href = '/auth/join'
+  }
+})
